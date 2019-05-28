5e7d0823c441434356b40333ecc41c04ad4d5a23
kylebirns <kylebirns@gmail.com>
2019-05-28 15:34:52 -0400

    finished jumpman

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,14 @@
 
 // Complete this algo
 const minJumps = arr => {
-
+    let jumpArr = arr.map(elem => Infinity)
+    jumpArr[0] = 0
+    for (let i = 1; i < jumpArr.length; i++){
+        for (let j = 0; j < arr[i - 1]; j++){
+            jumpArr[i + j] = Math.min(jumpArr[i - 1] + 1, jumpArr[i + j])
+        }
+    }
+    return jumpArr[arr.length - 1]
 };
 
 module.exports = minJumps
